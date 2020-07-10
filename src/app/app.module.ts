5f680d89209178fc65f9a7469563bc0b3bb64090
Arda İpek <arda.ipek@primeapps.io>
2020-07-10 17:12:42 +0300

    component home structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/home/menu/menu.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { CategoriesComponent } from './components/home/categories/categories.component';
+import { FeaturedProductsComponent } from './components/home/featured-products/featured-products.component';
+import { FooterComponent } from './components/home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    HeroComponent,
+    CategoriesComponent,
+    FeaturedProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
