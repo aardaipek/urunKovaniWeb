4c952e27546d4d607dd4a09cfd71bd2810889bbf
Arda İpek <arda.ipek@primeapps.io>
2020-07-15 15:33:20 +0300

    pages and components updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ShopDetailComponent } from './pages/shop/shop-detail/shop-detail.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "", pathMatch: "full", component: HomeComponent },
+  {
+    path: "shop", component: ShopComponent
+  },
+  { path: "detail/:id", component: ShopDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
