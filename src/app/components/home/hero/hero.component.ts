122e078d9b5890f8e201fc20373299cd4ce101fe
Atakan Gemici <atakan.gemici@ofisim.com>
2020-07-30 16:17:29 +0300

    template düzeltmeleri

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,defineInjectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {AppService} from "../../../app.service"
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -11,9 +12,18 @@ import {AppService} from "../../../app.service"
 export class HeroComponent implements OnInit {
   sideMenu : object;
   MenuCount : number;
+  categoryShow : boolean;
+  url : string;
 
-  constructor(private http: HttpClient,public appService : AppService) { 
+  constructor(private http: HttpClient,public appService : AppService,public route: ActivatedRoute) { 
     
+  }
+
+  categoryClose(categoryStatus){
+    if(categoryStatus)
+    this.categoryShow= false;
+    else
+    this.categoryShow = true;
   }
 
   async getMenus(){
@@ -23,6 +33,11 @@ export class HeroComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMenus();
+    this.url = this.route.routeConfig.path;
+
+    if(!this.url)
+    this.categoryShow = true;
+
   }
 
  
