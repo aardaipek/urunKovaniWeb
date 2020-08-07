8e13be6c010e102f830e1f158de5fef2bad92f39
Atakan Gemici <atakan.gemici@ofisim.com>
2020-08-07 20:18:06 +0300

    shop - cart component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ShopDetailComponent } from './pages/shop/shop-detail/shop-detail.component';
+import { ShopCartComponent } from './pages/shop/shoping-cart/shoping-cart.component';
 
 
 
@@ -10,6 +11,9 @@ const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
   {
     path: "shop", component: ShopComponent
+  },
+  {
+    path: "shop-cart", component: ShopCartComponent
   },
   { path: "detail/:id", component: ShopDetailComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BreadcrumbHeroComponent } from './components/shop/breadcrumb-hero/breadcrumb-hero.component';
 import { ShopDetailComponent } from './pages/shop/shop-detail/shop-detail.component';
+import { ShopCartComponent } from './pages/shop/shoping-cart/shoping-cart.component';
+
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -30,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     ShopComponent,
     HomeComponent,
     BreadcrumbHeroComponent,
-    ShopDetailComponent
+    ShopDetailComponent,
+    ShopCartComponent
   ],
   imports: [
     BrowserModule,
