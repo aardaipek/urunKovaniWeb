cc9f0bcbd7e8a7d50fe62bee04c4cd608aa33481
Arda İpek <arda.ipek@primeapps.io>
2020-08-10 17:23:55 +0300

    likes component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LikesComponent } from './components/likes/likes.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ShopDetailComponent } from './pages/shop/shop-detail/shop-detail.component';
 import { ShopingCartComponent } from './pages/shop/shoping-cart/shoping-cart.component';
@@ -9,13 +10,10 @@ import { ShopingCartComponent } from './pages/shop/shoping-cart/shoping-cart.com
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
-  {
-    path: "shop", component: ShopComponent
-  },
-  {
-    path: "shoping-cart", component: ShopingCartComponent
-  },
-  { path: "detail/:id", component: ShopDetailComponent }
+  { path: "shop", component: ShopComponent},
+  { path: "shoping-cart", component: ShopingCartComponent},
+  { path: "detail/:id", component: ShopDetailComponent },
+  { path: "likes/:id",component: LikesComponent} // id => userId olablir
 ];
 
 @NgModule({
