7afdb45810d6258e0d1d0ece3ddedbdd7150633c
Arda İpek <arda.ipek@primeapps.io>
2020-08-17 14:37:31 +0300

    register fixed

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { ToastService } from './toast.service';
 
 
 @Injectable({
@@ -12,7 +13,8 @@ export class AuthService {
 
   constructor(
     public http: HttpClient,
-    public route: Router,
+    public router: Router,
+    private toast: ToastService
   ) {
     this.url = 'https://localhost:44388';
   }
@@ -25,7 +27,12 @@ export class AuthService {
     params = params.append('password', data["password"]); 
     // memberType ve acceptRule eklenecek 
     // db de alanları açılmalı
-    const response = await this.http.post(this.url + '/api/user/register', { params: params }).toPromise();
+    const response = await this.http.post(this.url + '/api/app/register', data).toPromise().then((res)=> {
+        if(res != null){
+          this.toast.succesProcess("Kayıt başarılı")
+          this.router.navigateByUrl("/");
+        }
+    });
 
     return response;
   }
